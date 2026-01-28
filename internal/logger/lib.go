4b69ca5f60abbf95b95cd6e42df8ae18753049fb
Evan Lezar <elezar@nvidia.com>
2026-01-28 13:11:05 +0100

    Add internal logger package
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

---
+++ internal/logger/lib.go
@@ -0,0 +1,38 @@
+/**
+# Copyright (c) NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+**/
+
+package logger
+
+const (
+	Discard = discardLogger("discard")
+)
+
+// A discardLogger is a logger that logs nothing.
+type discardLogger string
+
+var _ Interface = (*discardLogger)(nil)
+
+// Debugf is a no-op for the discard logger.
+func (l discardLogger) Debugf(string, ...interface{}) {}
+
+// Errorf is a no-op for the discard logger.
+func (l discardLogger) Errorf(string, ...interface{}) {}
+
+// Infof is a no-op for the discard logger.
+func (l discardLogger) Infof(string, ...interface{}) {}
+
+// Warningf is a no-op for the discard logger.
+func (l discardLogger) Warningf(string, ...interface{}) {}
